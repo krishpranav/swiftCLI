c4ea1f8f4c1cc869a820e2fc4bcb4997e2ee9596
krishpranav <krisna.pranav@gmail.com>
2021-11-17 13:27:15 +0530

    swiftCLI: extension

--- swiftCLI/main.swift
+++ swiftCLI/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
 
+extension Process {
+    @discardableResult func launchBash(withCommand command: String) -> String? {
+        launchPath = "/bin/bash"
+        arguments = ["-c", command]
+        
+        let pipe = Pipe()
+        standardOutput = pipe
+        
+        standardError = Pipe()
+        
+        launch()
+        waitUntilExit()
+        
+        let outputData = pipe.fileHandleForReading.readDataToEndOfFile()
+        return String(data: outputData, encoding: .utf8)?.nonEmpty
+    }
+    
+    func gitConfigValue(forKey key: String) -> String? {
+        return launchBash(withCommand: "git config --global --get \(key)")?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
