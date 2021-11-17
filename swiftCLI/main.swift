23c808d477f5681a6f1da8fad02c3f1bf6cbf3c3
krishpranav <krisna.pranav@gmail.com>
2021-11-17 16:19:04 +0530

    swiftCLI: string extension

--- swiftCLI/main.swift
+++ swiftCLI/main.swift
@@ -29,3 +29,22 @@ extension Process {
         return launchBash(withCommand: "git config --global --get \(key)")?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
+
+extension String {
+    var nonEmpty: String? {
+        guard count > 0 else {
+            return nil
+        }
+        
+        return self
+    }
+    
+    func withoutSuffix(_ suffix: String) -> String {
+        guard hasSuffix(suffix) else {
+            return self
+        }
+        
+        let startIndex = index(endIndex, offsetBy: -suffix.count)
+        return replacingCharacters(in: startIndex..<endIndex, with: "")
+    }
+}
